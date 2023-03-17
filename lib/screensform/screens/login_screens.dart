7e17332c412c61a74f7657eca1fa8b4b87066696
saidIDLKitty <114540627+saidIDLKitty@users.noreply.github.com>
2023-03-16 20:39:17 -0500

    new

--- lib/screensform/screens/login_screens.dart
+++ lib/screensform/screens/login_screens.dart
@@ -6,18 +6,27 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  //Variable de tipo FormState
+  //La variable formkey obtenos una referencia del FormState
+  //Accedemos con variable a los valores que contengan los input
+  final GlobalKey<FormState> formkey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return new Container(
       margin: EdgeInsets.all(20.0),
       child: Form(
+          key: formkey,
           child: Column(
-        children: <Widget>[
-          emailField(),
-          passwordField(),
-          submitField(),
-        ],
-      )),
+            children: <Widget>[
+              emailField(),
+              passwordField(),
+              Container(
+                margin: EdgeInsets.only(top: 25.0),
+              ),
+              submitField(),
+            ],
+          )),
     );
   }
 
@@ -26,26 +35,35 @@ class _LoginState extends State<Login> {
       keyboardType: TextInputType.emailAddress,
       decoration:
           InputDecoration(labelText: 'Email', hintText: 'example@dominio.com'),
+      validator: (value) {
+        //print(value);
+        if (!value!.contains("@")) {
+          return "email invalido";
+        }
+      },
     );
   }
 
   Widget passwordField() {
-    return Container(
-      margin: EdgeInsets.only(bottom: 25.0),
-      child: TextFormField(
+    return TextFormField(
       obscureText: true,
       decoration: InputDecoration(
         labelText: 'Contraseña',
         hintText: 'Contraseña',
       ),
-      ),
+      validator: (value) {
+        print(value);
+        if (value!.length < 6) return "Contraseña Invalida";
+      },
     );
   }
 
   Widget submitField() {
     return ElevatedButton(
       child: Text("Enviar"),
-      onPressed: () {},
+      onPressed: () {
+        formkey.currentState!.validate();
+      },
     );
   }
 }
